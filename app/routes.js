2915293f61e18e9997f66c72a470584e37ef8dbb
akshatrb <thecyberhuman@gmail.com>
2021-04-06 12:22:17 +0530

    fixed updateNoteById PUT

--- app/routes.js
+++ app/routes.js
@@ -3,6 +3,6 @@ const controller = require('./controller');
 module.exports = (app) => {
     app.post('/api/note', controller.createNewNote);
     app.get('/api/notes/:type', controller.getAllNotes);
-    app.put('/api/notes/:id', controller.updateNoteById);
+    app.put('/api/note/:id', controller.updateNoteById);
     app.delete('/api/note/:id', controller.deleteNote);
-}
+};
