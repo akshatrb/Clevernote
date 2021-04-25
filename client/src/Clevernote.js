3d31de3bf1842673ea702ebd301cd5e1433e5d90
akshatrb <thecyberhuman@gmail.com>
2021-04-25 10:21:04 +0530

    added route to /

--- client/src/Clevernote.js
+++ client/src/Clevernote.js
@@ -1,9 +1,10 @@
 import React, { useReducer } from 'react';
 import './Clevernote.scss';
+import Home from './components/Home/Home';
 import Sidenavbar from './components/Sidenavbar/Sidenavbar';
 import NoteList from './components/NoteList/NoteList';
 import Note from './components/Note/Note';
-import{
+import {
   BrowserRouter as Router,
   Switch,
   Route
@@ -12,21 +13,26 @@ import NoteReducer from './reducer/NoteReducer';
 import { NotesContext } from './context/context';
 
 const initialState = [];
- 
+
 //We've changed the orientation on line 24 where <Note /> !! If something breaks, revert from Master branch
 //Wrapping our div with Router
 
 function Clevernote() {
   const [notes, notesDispatch] = useReducer(NoteReducer, initialState); //react hook useReducer, passing NoteReducer and initial state into it.
-  
+
   //wrapping everything with notesContext.provider inside router
-  
+
   return (
     <Router>
-      <NotesContext.Provider value={{notesState: notes, notesDispatch}}>
-        <div className="Clevernote">
-          <Sidenavbar />
-          <Switch>
+      <Switch>
+        <Route exact path="/">
+          <Home title="Home" />
+        </Route>
+
+        <NotesContext.Provider value={{ notesState: notes, notesDispatch }}>
+          <div className="Clevernote">
+            <Sidenavbar />
+
             <Route path="/all-notes">
               <NoteList title="All Notes" />
               <Route path="/all-notes/:id">
@@ -39,9 +45,10 @@ function Clevernote() {
                 <Note />
               </Route>
             </Route>
-          </Switch>
-        </div>
-      </NotesContext.Provider>
+
+          </div>
+        </NotesContext.Provider>
+      </Switch>
     </Router>
   );
 }
