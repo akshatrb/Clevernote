3e722c2d437a2ffcd1aaf10f23e6d94a26d4e396
akshatrb <thecyberhuman@gmail.com>
2021-05-04 01:04:54 +0530

    prod

--- client/src/utils/apiEndpoints.js
+++ client/src/utils/apiEndpoints.js
@@ -1,6 +1,6 @@
 //we will have all our api endpoints here
 
-export const BASE_URL = 'http://localhost:2000'; // our server is running on port 2000
+export const BASE_URL = `${process.env.BASE_URL}:${process.env.port}`; // our server is running on port 2000
 export const CREATE_NOTE = '/api/note';
 export const UPDATE_NOTE = '/api/note/';
 export const DELETE_NOTE = '/api/note/';
